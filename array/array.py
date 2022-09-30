ae24a14786a93b28158aa0e74429e1c49a71cb4d
ZTStew <ZT.Stewart.5@gmail.com>
2022-09-30 09:57:53 -0400

    First commit

---
+++ array/array.py
@@ -0,0 +1,28 @@
+"""
+
+Arrays
+
+how do you track multiple values in programming?
+
+let's say that you are tracking 3 peoples names. How would you do so?
+"""
+
+# this won't work as it 
+name = "zach"
+name = "thomas"
+name = "stewart"
+print(name)
+
+# this will allow us to record each name by storing each in a different variable
+name1 = "zach"
+name2 = "thomas"
+name3 = "stewart"
+
+# but must call each name by the variable,
+print(name1)
+print(name2)
+print(name3)
+
+# this solution is not expandable or maintainable
+# solution? store multiple values in one variable
+
